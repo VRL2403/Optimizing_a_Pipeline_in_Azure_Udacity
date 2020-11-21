d344fc11c68e709c8605ee87ab34f339fd0b14ce
viral_mamniya <vnmamniya@gmail.com>
2020-11-21 16:28:28 +0530

    Training script and Notebook updated

--- train.py
+++ train.py
@@ -13,14 +13,15 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+dataset_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files(path=dataset_path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=0)
 
 run = Run.get_context()
 
@@ -49,7 +50,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
 
 def main():
     # Add arguments to script
